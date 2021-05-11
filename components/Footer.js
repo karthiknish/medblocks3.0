9a1692b63a993a0a85299039a7b41e21bb61baab
Karthik Nishanth <karthik.nishanth06@gmail.com>
2021-05-11 20:14:00 +0530

    'consult'

--- components/Footer.js
+++ components/Footer.js
@@ -1,12 +1,18 @@
 export default function Footer() {
   return (
-      <div className="flex bg-gray-100 p-10 justify-center">
-        <p>© 2021 Medblocks. All rights reserved. <wbr/>For communications email <a
-            className="font-bold"
-            className="text-blue-500 font-semibold hover:text-blue-400"
-            href="mailto:sidharth@medblocks.com">
-            sidharth@medblocks.com
-          </a>.</p>
-      </div>
+    <div className="flex bg-gray-100 p-10 justify-center">
+      <p>
+        © 2021 Medblocks. All rights reserved. <wbr />
+        For communications email{" "}
+        <a
+          className="font-bold"
+          className="text-blue-500 font-semibold hover:text-blue-400"
+          href="mailto:sidharth@medblocks.com"
+        >
+          sidharth@medblocks.com
+        </a>
+        .
+      </p>
+    </div>
   );
 }
