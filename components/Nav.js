970eef2838f12383b4174d807282e81b4141b8ae
Sidharth Ramesh <tornadoalert@gmail.com>
2021-06-19 17:13:20 +0530

    changed blog link back

--- components/Nav.js
+++ components/Nav.js
@@ -69,7 +69,7 @@ export default function Nav() {
                     rel="noreferrer"
                     target="_blank"
                     className="button px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-gray-700  hover:bg-gray-200 rounded"
-                    href="https://medblocks.medium.com/"
+                    href="https://blog.medblocks.org/"
                   >
                     Blog
                   </a>
